e83c9ca9d83c9ff2c7e9bc01abfe5ba6dd325d6a
= <=>
2017-07-29 13:49:39 +0800

    fixbug

--- src/main/java/org/igeek/service/impl/QualityCollectServiceImpl.java
+++ src/main/java/org/igeek/service/impl/QualityCollectServiceImpl.java
@@ -309,7 +309,7 @@ public class QualityCollectServiceImpl implements IQualityCollectService {
                 qualityVo.setQualityIdName(quality.getTitle());
                 qualityVo.setCollectType(quality.getQuestionType());
                 qualityVo.setQualityId(quality.getId());
-                qualityVo.setQuestionType(quality.getQuestionType());
+                qualityVo.setQuestionType(quality.getUserId());
                 qualityVoList.add(qualityVo);
             }
             return ServerResponse.createBySuccess("获取质量问题列表成功", qualityVoList);
